f990f53957cfbcc178c28fcaacf28a645ba4f3d2
psalms4 <psalms.mutiso@strathmore.edu>
2025-12-16 12:56:23 +0100

    Assigment is done.

--- estimate_pi.py
+++ estimate_pi.py
@@ -1,7 +1,24 @@
+#Write a function:
+#`estimate_pi(num_samples)`
+#that:
+#returns an estimate of π using num_samples random points generated with np.random.rand().
+import numpy as np
+
+
 def estimate_pi(num_samples):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    count = 0
+    
+    for i in range(num_samples):
+        x = np.random.rand()
+        y = np.random.rand()
+        
+        if x**2 + y**2 <= 1:
+            count +=1
+            
+    pi_estimate = 4* count/num_samples
+    return pi_estimate
+    
+    
+    
+
+    
--- random_unit_vectors.py
+++ random_unit_vectors.py
@@ -1,7 +1,26 @@
+#Write a function:
+#`random_unit_vectors(num_vectors, dim)`
+#that:
+#a)creates a matrix M of shape (num_vectors, dim)using;
+#M = np.random.randn(num_vectors, dim)
+#b)normalizes each row so it has Euclidean norm 1,
+#and c)returns the resulting matrix as a NumPy array.
+
+
+import numpy as np
+
 def random_unit_vectors(num_vectors, dim):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    M = np.random.randn(num_vectors, dim)
+    
+    l2 = np.linalg.norm(M, axis = 1,keepdims = True)
+    
+    M_norms = M / l2
+    
+    return M_norms
+       
+
+
+    
+    
+    
+   
